636efe6ce6de9ed2980baf23492c5ebba02feb59
Omayma El kasbaoui <omayma.el-kasbaoui@etu.mines-ales.fr>
2025-01-14 16:22:40 +0100

    feat : add contact test

---
+++ test-end-to-end/examples/contact/contact_test.js
@@ -0,0 +1,44 @@
+describe('Contact Page Test', function() {
+  it('should load the contact page and check the header text - Chrome', function(browser) {
+    browser
+      .navigateTo('http://localhost:9090/contact')
+      .waitForElementVisible('body', 1000)
+      .assert.visible('header.container-fluid')
+      .assert.textContains('.home--header-title', 'CONTACTEZ-NOUS')
+      .assert.textContains('.display-6.text-danger', 'remplissez le formulaire')
+      .assert.visible('form')
+      .assert.visible('input[name="firstName"]')
+      .assert.visible('input[name="lastName"]')
+      .assert.visible('input[name="mobilePhone"]')
+      .assert.visible('input[name="arrivedAt"]')
+      .assert.visible('input[name="departureAt"]')
+      .end();
+  });
+
+  it('should fill out the contact form and submit - Chrome', function(browser) {
+    browser
+      .navigateTo('http://localhost:9090/contact')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[name="firstName"]', 'Jean')
+      .setValue('input[name="lastName"]', 'Louis')
+      .setValue('input[name="email"]', 'jean.louis@example.com')
+      .setValue('input[name="mobilePhone"]', '0610203040')
+      .setValue('input[name="arrivedAt"]', '2023-12-01')
+      .setValue('input[name="departureAt"]', '2023-12-10')
+      .setValue('textarea[name="message"]', 'This is a test message.')
+      .pause(1000)
+      .execute(function() {
+        document.querySelector('button[type="submit"]').scrollIntoView();
+      })
+      .click('button[type="submit"]')
+      .pause(1000)
+      .assert.urlContains('/contact')
+      .assert.value('input[name="firstName"]', '')
+      .assert.value('input[name="lastName"]', '')
+      .assert.value('input[name="mobilePhone"]', '')
+      .assert.value('input[name="arrivedAt"]', '')
+      .assert.value('input[name="departureAt"]', '')
+      .assert.value('textarea[name="message"]', '')
+      .end();
+  });
+});
