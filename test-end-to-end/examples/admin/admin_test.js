d291aa1f34ff8f9832bc087b78ef5928441d9201
Omayma El kasbaoui <omayma.el-kasbaoui@etu.mines-ales.fr>
2025-01-17 16:26:30 +0100

    feat : add admin tests

---
+++ test-end-to-end/examples/admin/admin_test.js
@@ -0,0 +1,59 @@
+describe('Admin Page Test', function() {
+    const loginUrl = 'http://localhost:9090/login';
+    const adminUrl = 'http://localhost:9090/admin';
+  
+    function login(browser) {
+      browser
+        .windowMaximize()
+        .navigateTo(loginUrl)
+        .useCss()
+        .waitForElementVisible('body', 10000)
+        .useXpath()
+        .setValue('//form//input[@name="name"]', 'Soizic')
+        .setValue('//form//input[@name="password"]', 'Vernet')
+        .click('//form//button[@type="submit" and contains(@class, "btn btn-primary")]')
+        .waitForElementVisible('//body', 10000)
+        .assert.urlContains('/admin');
+    }
+  
+    it('should load the admin page and check the main container', function(browser) {
+      login(browser);
+      browser
+        .navigateTo(adminUrl)
+        .useCss()
+        .waitForElementVisible('body', 10000)
+        .useXpath()
+        .assert.elementPresent('//main[contains(@class, "container mt-5")]')
+        .end();
+    });
+  
+    it('should check the presence of the main content area', function(browser) {
+      login(browser);
+      browser
+        .navigateTo(adminUrl)
+        .useXpath()
+        .assert.elementPresent('//main[contains(@class, "container mt-5")]')
+        .end();
+    });
+  
+    it('should check the presence of the admin panel elements', function(browser) {
+      login(browser);
+      browser
+        .navigateTo(adminUrl)
+        .useXpath()
+        .assert.elementPresent('/html/body/main/div[2]')
+        .assert.elementPresent('/html/body/main/div[2]/div/table/tbody')
+        .end();
+    });
+  
+    it('should check the functionality of the "Disconnect" button', function(browser) {
+        login(browser);
+        browser
+          .navigateTo(adminUrl)
+          .useXpath()
+          .click('/html/body/main/div[1]/nav/div/div/ul/li/button')
+          .waitForElementVisible('//body', 10000)
+          .assert.urlContains('/login')
+          .end();
+    });
+  });
